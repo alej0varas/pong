a9b67542c8b649440329a3829a0ed49d9d39db5e
Alejandro Varas <alej0varas@gmail.com>
2010-06-24 18:28:24 -0400

    Events OK

--- pong.py
+++ pong.py
@@ -10,17 +10,32 @@ class Pong:
     def __init__(self):
         self.playing = True
         pygame.display.init()
+        pygame.event.set_allowed(None)
+        pygame.event.set_allowed(pygame.KEYDOWN)
 
     def run(self):
-        pygame.display.set_mode(config.resolution)
         # display
+        pygame.display.set_mode(config.resolution)
+
         while self.playing:
             # events
+            key = pygame.event.wait().key
+            # check quit
+            if key == pygame.K_q:
+                self.quit()
+                continue
             # player
+            if key == pygame.K_UP:
+                print "up"
+            elif key == pygame.K_DOWN:
+                print "down"
             # ai
             # sprites
-            # check quit
-            self.playing = False
+            # screen update, flip
+#            print self
+
+    def quit(self):
+        self.playing = False
 
     def __str__(self):
         return str(self.playing)
