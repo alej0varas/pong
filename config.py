10068efa3eb1f1d0bd266e5fff9ca3abb79018a9
Alejandro Varas <alej0varas@gmail.com>
2010-06-24 23:15:10 -0400

    Ball collide top and botom

--- config.py
+++ config.py
@@ -1,4 +1,10 @@
 # pong config
 
-resolution = (640, 480)
+width = 640
+height = 480
+resolution = (width, height)
+ball_color = (255, 255, 255)
+background_color = (0, 0, 0)
+vel_x = 5
+vel_y = 5
 
--- pong.py
+++ pong.py
@@ -11,13 +11,14 @@ class Pong:
         self.playing = True
         self.player = Player()
         self.ia = IA()
+        self.ball = Ball(self)
         pygame.display.init()
         pygame.event.set_allowed(None)
         pygame.event.set_allowed(pygame.KEYDOWN)
 
     def run(self):
         # display
-        pygame.display.set_mode(config.resolution)
+        self.display = pygame.display.set_mode(config.resolution)
 
         while self.playing:
             # events
@@ -30,7 +31,11 @@ class Pong:
             self.player.play(key)
             # ai
             self.ia.play()
+            # draw
+            self.ball.play()
+
             # screen update, flip
+            pygame.display.flip()
 #            print self
 
     def quit(self):
@@ -42,7 +47,7 @@ class Pong:
 
 class Player:
     def __init__(self):
-        self.pad = Pad(self)
+        self.pad = Pad(self, 10, 100)
 
     def play(self, key):
         if key == pygame.K_UP:
@@ -53,7 +58,7 @@ class Player:
 
 class IA:
     def __init__(self):
-        self.pad = Pad(self)
+        self.pad = Pad(self, 100, 100)
         #super(type(IA), self).__init__()
 
     def play(self):
@@ -66,13 +71,40 @@ class Pad:
         self.owner = owner
         self.x = x
         self.y = y
+        self.rect = pygame.Rect(0, 0, 0, 0)
         
     def up(self):
+        self.y += 1
         print "up"
 
     def down(self):
+        self.y -= 1
         print "down"
 
+
+class Ball:
+    def __init__(self, game):
+        self.game = game
+        self.x, self.y = config.width/2, config.height/2
+        self.vel_x = config.vel_x * 0
+        self.vel_y = config.vel_y * -1
+        self.rect = pygame.Rect(0, 0, 0, 0)
+
+    def play(self):
+        print self.rect.top, self.rect.bottom
+        if self.rect.top <= 0 or self.rect.bottom >= config.height:
+            self.vel_y *= -1
+
+        if self.rect.collidelist((self.game.player.pad.rect, self.game.ia.pad.rect)) is not -1:
+            self.vel_x *= -1
+
+        self.x += self.vel_x
+        self.y += self.vel_y
+        self.draw()
+
+    def draw(self):
+        pygame.draw.rect(self.game.display, config.background_color, self.rect)
+        self.rect = pygame.draw.circle(self.game.display, config.ball_color, (self.x, self.y), 20)
 
 def main():
     p = Pong()
