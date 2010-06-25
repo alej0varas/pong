415a49667edcb4ad45a87c268399bfcb1067d463
Alejandro Varas <alej0varas@gmail.com>
2010-06-25 01:00:47 -0400

    Pad draw, winer check, show score

--- config.py
+++ config.py
@@ -2,9 +2,19 @@
 
 width = 640
 height = 480
+pad_width = 10
+pad_height = 60
+pad_speed = 10
 resolution = (width, height)
 ball_color = (255, 255, 255)
 background_color = (0, 0, 0)
 vel_x = 5
 vel_y = 5
-
+init_y = height / 2
+ball_ratio = 20
+ball_init_x = width / 2 - ball_ratio
+ball_init_y = height / 2
+line_width = 10
+line_init_x = ball_init_x + line_width
+score_y = 10
+score_text_size = 30
--- pong.py
+++ pong.py
@@ -9,18 +9,21 @@ import config
 class Pong:
     def __init__(self):
         self.playing = True
-        self.player = Player()
-        self.ia = IA()
+        self.player = Player(self)
+        self.ia = IA(self)
+        self.board = Board(self)
         self.ball = Ball(self)
+        self.winer = 0
         pygame.display.init()
+        pygame.font.init()
         pygame.event.set_allowed(None)
         pygame.event.set_allowed(pygame.KEYDOWN)
-
+        
     def run(self):
         # display
         self.display = pygame.display.set_mode(config.resolution)
-
         while self.playing:
+            self.board.draw()
             # events
             key = pygame.event.wait().key
             # check quit
@@ -33,21 +36,39 @@ class Pong:
             self.ia.play()
             # draw
             self.ball.play()
-
+            self.player.pad.draw()
+            self.ia.pad.draw()
+            # check winer
+            self.check_winer()
             # screen update, flip
             pygame.display.flip()
-#            print self
+
+    def check_winer(self):
+        if self.winer > 0:
+            self.player.score += 1
+        elif self.winer < 0:
+            self.ia.score += 1
+        if self.winer:
+            self.winer = 0
+            self.restart()
+
+    def restart(self):
+        self.display.fill(config.background_color)
+        self.board.draw()
+        self.ball.rect.center = (config.ball_init_x, config.ball_init_y)
+        self.ball.draw()
+        self.player.pad.centerx = config.init_y
+        self.ia.pad.centery = config.init_y
 
     def quit(self):
         self.playing = False
 
-    def __str__(self):
-        return str(self.playing)
-
 
 class Player:
-    def __init__(self):
+    def __init__(self, game):
+        self.game = game
         self.pad = Pad(self, 10, 100)
+        self.score = 0
 
     def play(self, key):
         if key == pygame.K_UP:
@@ -57,54 +78,77 @@ class Player:
 
 
 class IA:
-    def __init__(self):
+    def __init__(self, game):
+        self.game = game
         self.pad = Pad(self, 100, 100)
-        #super(type(IA), self).__init__()
+        self.score = 0
 
     def play(self):
-        print "ia turn"
-        pass
+        if self.game.ball.rect.centery < self.pad.rect.centery:
+            self.pad.up()
+        else:
+            self.pad.down()
 
 
 class Pad:
     def __init__(self, owner, x, y):
         self.owner = owner
-        self.x = x
-        self.y = y
-        self.rect = pygame.Rect(0, 0, 0, 0)
+        self.rect = pygame.Rect(x, y, config.pad_width, config.pad_height)
+        self._rect = self.rect.copy()
         
     def up(self):
-        self.y += 1
-        print "up"
+        self.rect.top -= config.pad_speed
 
     def down(self):
-        self.y -= 1
-        print "down"
+        self.rect.top += config.pad_speed
 
+    def draw(self):
+        pygame.draw.rect(self.owner.game.display, config.background_color, self._rect)
+        pygame.draw.rect(self.owner.game.display, config.ball_color, self.rect)
+        self._rect = self.rect.copy()
 
 class Ball:
     def __init__(self, game):
         self.game = game
-        self.x, self.y = config.width/2, config.height/2
-        self.vel_x = config.vel_x * 0
-        self.vel_y = config.vel_y * -1
-        self.rect = pygame.Rect(0, 0, 0, 0)
+        self.vel_x = config.vel_x
+        self.vel_y = config.vel_y
+        self.rect = pygame.Rect(config.ball_init_x, config.ball_init_y, 0, 0)
+        self._rect = self.rect.copy()
 
     def play(self):
-        print self.rect.top, self.rect.bottom
         if self.rect.top <= 0 or self.rect.bottom >= config.height:
             self.vel_y *= -1
 
         if self.rect.collidelist((self.game.player.pad.rect, self.game.ia.pad.rect)) is not -1:
             self.vel_x *= -1
 
-        self.x += self.vel_x
-        self.y += self.vel_y
+        if self.rect.left <= 0 or self.rect.right >= config.width:
+            self.game.winer = self.vel_x
+
+        self._rect = self.rect.copy()
+        self.rect.centerx += self.vel_x
+        self.rect.centery += self.vel_y
         self.draw()
 
     def draw(self):
-        pygame.draw.rect(self.game.display, config.background_color, self.rect)
-        self.rect = pygame.draw.circle(self.game.display, config.ball_color, (self.x, self.y), 20)
+        pygame.draw.rect(self.game.display, config.background_color, self._rect)
+        self.rect = pygame.draw.circle(
+            self.game.display, config.ball_color, (self.rect.centerx, self.rect.centery), config.ball_ratio)
+
+
+class Board:
+    def __init__(self, game):
+        self.game = game
+        self.line = pygame.Rect(config.line_init_x, 0, config.line_width, config.height)
+
+    def draw(self):
+        pygame.draw.rect(self.game.display, config.ball_color, self.line)
+        self.font = pygame.font.Font(pygame.font.get_default_font(), config.score_text_size)
+        self.player = self.font.render(str(self.game.player.score), 1, config.ball_color)
+        self.ia = self.font.render(str(self.game.ia.score), 1, config.ball_color)
+
+        self.game.display.blit(self.player, (config.score_text_size, config.score_y))
+        self.game.display.blit(self.ia, (config.width - config.score_text_size, config.score_y))
 
 def main():
     p = Pong()
